b8e4c78c1f2a76f442d3e0fad091623efdebf48d
VishnuNair1903 <vishn1903@gmail.com>
2024-05-14 15:44:03 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [number, setNumber]=useState(0);
+  const [maximum, setMaximum]=useState(10);
+
+  function increaseNumber(){
+    if(number>maximum){
+      alert("Number can't be greater than "+maximum);
+      setNumber(maximum);
+    }else{
+      setNumber(number+1);
+    }
+  }
+
+  function decreaseNumber(){
+    if(number>0){
+      setNumber(number-1);
+    }else{
+      alert("Number can't be less than 0");
+      setNumber(0);
+    }
+  }
+
+  function handleMaximum(event){
+    const value=parseInt(event.target.value);
+    console.log(value);
+    setMaximum(value);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Enter the limit</p>
+      <input type="number" value={maximum} onChange={handleMaximum} className='max-input' />
+      <div className='number'>
+        {number}
+      </div>
+      <button className="btn" onClick={increaseNumber}>Increment</button>
+      <button className='btn' onClick={decreaseNumber}>Decrement</button>
+      
     </div>
   );
 }
